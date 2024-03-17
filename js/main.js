6bf3443c0194f42b2a36158d8d86f1b43acf85b8
Suraj Patil <suraj.gd89@gmail.com>
2024-03-17 10:39:11 +0530

    final code

--- js/main.js
+++ js/main.js
@@ -1,39 +1,75 @@
 $(document).ready(function () {
-    var tabs_Top = parseInt($('.tabs').css('top'), 10);
-    var tabs_Height = $('.tabs').outerHeight(true) + tabs_Top;
+    let tabsHeight =
+        $('.tabs').outerHeight(true) + parseInt($('.tabs').css('top'), 10);
 
-    function highlightTab(scroll_Top) {
-        $('.tab-content:not(.blank)').each(function (index, element) {
-            var target_Top = $(this).offset().top - tabs_Height;
-            var target_Bottom = target_Top + $(this).outerHeight(true);
+    $('.tab-content').each(function (index, element) {
+        let tabContent = $(this);
+        let targetTop = tabContent.offset().top;
 
-            if (scroll_Top >= target_Top && scroll_Top < target_Bottom) {
-                var id = $(this).attr('id');
-                $('.tab-link').removeClass('active');
-                $('.tab-link[href="#' + id + '"]').addClass('active');
-                $(this).addClass('active');
-                $(this).css({
-                    top: tabs_Height,
-                });
-            }
-        });
-    }
+        if (index !== 0) {
+            tabsHeight += 15;
+        }
+
+        $(this).css({ top: tabsHeight });
+        $(this).attr('target-top', targetTop);
+        $(this).attr('sticky-top', tabsHeight);
+    });
+
+    let lastTabIndex = $('.tab-content').length - 1;
+    const initialColor = [203, 227, 232];
 
     $(window).on('scroll', function () {
-        var scroll_Top = $(window).scrollTop();
-        highlightTab(scroll_Top);
+        let scrollTop = $(window).scrollTop();
+        $('.tab-content').each(function (index) {
+            let tabContent = $(this);
+            let targetTop = tabContent.offset().top - tabsHeight;
+            let targetBottom = targetTop + tabContent.outerHeight(true);
+            let activeRange = tabContent.outerHeight(true) - tabsHeight;
+            if (
+                scrollTop >= targetTop - activeRange &&
+                scrollTop < targetBottom
+            ) {
+                let id = tabContent.attr('id');
+                $('.tab-link').removeClass('active');
+                $('.tab-link[href="#' + id + '"]').addClass('active');
+                tabContent.addClass('active');
+                //SCALE
+                var distance = Math.max(
+                    0,
+                    Math.min(scrollTop - targetTop, targetBottom - targetTop)
+                );
+                var scale = 1 - distance / tabContent.outerHeight(true);
+                //BGCOLOR
+                let [r, g, b] = initialColor;
+                let intensity = Math.max(0, 255 - distance * 0.2);
+                let bgColor =
+                    'rgb(' +
+                    (r * intensity) / 255 +
+                    ', ' +
+                    (g * intensity) / 255 +
+                    ', ' +
+                    (b * intensity) / 255 +
+                    ')';
+
+                if (lastTabIndex !== index) {
+                    tabContent.css('transform', 'scale(' + scale + ')');
+                    tabContent.css('background-color', bgColor);
+                }
+            } else {
+                tabContent.removeClass('active');
+            }
+        });
     });
 
     $('.tab-link').on('click', function (e) {
         e.preventDefault();
-        var target = $($(this).attr('href'));
-        var scroll_Top = target.offset().top - tabs_Height;
-
-        highlightTab(scroll_Top);
-
-        $('html, body').animate(
+        let tabLink = $(this);
+        let tabContent = $(tabLink.attr('href'));
+        let targetTop =
+            tabContent.attr('target-top') - tabContent.attr('sticky-top');
+        $('html, body').stop().animate(
             {
-                scrollTop: scroll_Top,
+                scrollTop: targetTop,
             },
             500
         );
